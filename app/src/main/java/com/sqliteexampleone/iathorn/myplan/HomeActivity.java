6afdb88feda9b091458fb07da6d80f284c170b41
knobfler <donghochoi1496@gmail.com>
2018-05-17 10:45:58 +0900

    backup

--- app/src/main/java/com/sqliteexampleone/iathorn/myplan/HomeActivity.java
+++ app/src/main/java/com/sqliteexampleone/iathorn/myplan/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.sqliteexampleone.iathorn.myplan;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    Button googleMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        googleMaps = (Button) findViewById(R.id.googleMaps);
+
+        googleMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://maps.google.com/maps?f=d&saddr=53.447,-0.878&daddr=51.448,-0.972"));
+                intent.setComponent(new ComponentName("com.google.android.apps.maps",
+                        "com.google.android.maps.MapsActivity"));
+                startActivity(intent);
+            }
+        });
     }
 }
